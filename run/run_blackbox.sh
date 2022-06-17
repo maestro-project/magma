4525cffd1c424d4a9cc23af00f24d346b095bb95
Felix Kao <34088771+felix0901@users.noreply.github.com>
2022-06-17 13:39:35 -0400

    Update run_blackbox.sh

--- run/run_blackbox.sh
+++ run/run_blackbox.sh
@@ -1,2 +1,2 @@
 
-python ../src/main_ng.py  --epoch 10 --dram_bw 1 --outdir output_ng --setting 2 --instsdir batch_conv --instsfile insts4
+python ../src/main_ng.py  --epoch 10 --alg PSO --dram_bw 1 --outdir output_ng --setting 2 --instsdir batch_conv --instsfile insts4
